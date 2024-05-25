bfa56e397194335cc16f292c2c0b29c25e6ed8a9
dblatcher <dv.bltchr@gmail.com>
2024-05-25 11:30:49 +0100

    clutter

--- apps/bestagon-examples/src/app/app.tsx
+++ apps/bestagon-examples/src/app/app.tsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Board } from '../components/Board';
+import { Examples } from '../components/Examples';
 import { HexPathTest } from '../components/HexPathTest';
 
 export function App() {
   return (
     <>
       <Board rows={5} width={5} />
-      <HexPathTest rows={10} width={16} startLow showAxialCoords/>
+      <HexPathTest rows={10} width={16} evenColsLow showAxialCoords/>
+      <Examples />
     </>
   );
 }
